358566725494eb72f9f5c669546a2eb45edb0e89
Agshin H. Alakbarov <agshinha@code.local>
2022-12-09 14:00:40 +0400

    first comment

---
+++ task 1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            if (input == "salam")
+            {
+                Console.WriteLine("aleykum salam!");
+            }
+            else if (input == "sagol")
+            {
+                Console.WriteLine("sende sagol");
+            }
+            else
+            {
+                Console.WriteLine("RUsca bilmirem");
+            }
+           
+                   
+            
+        }
+    }
+}
---
+++ task 2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gunu elave edin");
+           var input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("birinci gun");
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("Ikinci gun");
+            }
+            else if (input == "3")
+            {
+                Console.WriteLine("uchuncu gun");
+            }
+            else if (input == "4")
+            {
+                Console.WriteLine("4 gun");
+            }
+            else if (input == "6")
+            {
+                Console.WriteLine("6 gun");
+            }
+            else if (input == "7")
+            {
+                Console.WriteLine("7 gun");
+            }
+            
+        
+                  
+        }
+    }
+}
